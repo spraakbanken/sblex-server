ec439ee2fbefe48fa25bb200f8e7b0e3170b8d89
Kristoffer Andersson <kristoffer.andersson@gu.se>
2023-03-27 11:11:01 +0200

    refactor: lemma xmlize and htmlize to templates

--- src/sblex/lemma.py
+++ src/sblex/lemma.py
@@ -32,28 +32,3 @@ def function(format, lemma):
     except:
         result_code = apache.HTTP_SERVICE_UNAVAILABLE
     return (result, result_code)
-
-
-def xmlize(s):
-    j = cjson.decode(utf8.d(s))
-    xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
-    xml += "<result>\n"
-    if not (j == []):
-        xml += " <gf>" + j["gf"] + "</gf>\n"
-        xml += " <p>" + j["p"] + "</p>\n"
-        xml += " <ls>\n"
-        for l in j["l"]:
-            xml += "   <l>" + l + "</l>\n"
-        xml += " </ls>\n"
-    xml += "</result>\n"
-    return utf8.e(xml)
-
-
-def htmlize(lid, s):
-    j = cjson.decode(utf8.d(s))
-    if not (j == []):
-        return table.function("html", utf8.e(j["p"]), utf8.e(j["gf"]))[0]
-    else:
-        content = "<center><p><b>" + lid + " finns ej.</b></p></center>"
-        html = saldo_util.html_document(lid, content, bar=False)
-        return html
