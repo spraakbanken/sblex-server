04a093fad46eff3be8747626273ddeedd69639f3
Kristoffer Andersson <kristoffer.andersson@gu.se>
2023-03-27 13:59:43 +0200

    refactor: move prlex to formatting

--- src/sblex/saldo_util.py
+++ src/sblex/saldo_util.py
@@ -201,23 +201,6 @@ def inits(s):
     return xs
 
 
-def prlex(lex):
-    return "+".join([_prlex(l) for l in lex.split()])
-
-
-def _prlex(lex):
-    try:
-        s = lex[:-3]
-        if lex[-1] == "1":
-            return s
-        elif lex[-2] == ".":
-            return s + "<sup>" + lex[-1] + "</sup>"
-        else:
-            return lex
-    except:
-        return lex
-
-
 def lemma(l):
     rl = l[::-1]
     i = rl.find("..")
