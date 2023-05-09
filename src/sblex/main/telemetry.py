111767d4c380fd53c9c94889ab0e02e091f7d755
Kristoffer Andersson <kristoffer.andersson@gu.se>
2023-05-09 10:51:41 +0200

    fix: set log level to INFO

--- src/sblex/main/telemetry.py
+++ src/sblex/main/telemetry.py
@@ -1,3 +1,4 @@
+import logging
 from logging.config import dictConfig
 
 from fastapi import FastAPI
@@ -24,7 +25,7 @@ def setting_otlp(app: FastAPI, app_name: str, *, log_correlation: bool = True) -
     trace.set_tracer_provider(tracer)
 
     if log_correlation:
-        LoggingInstrumentor().instrument(set_logging_format=False)
+        LoggingInstrumentor(log_level=logging.INFO).instrument(set_logging_format=False)
 
     FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer)
 
@@ -69,7 +70,7 @@ def configure_logging(settings: dict[str, str], *, use_telemetry: bool = True) -
             "loggers": {
                 "sblex": {
                     "handlers": ["json"],
-                    "level": "DEBUG",
+                    "level": "INFO",
                     "propagate": True,
                 },
                 # third-party package loggers
