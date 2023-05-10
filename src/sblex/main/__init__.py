54d810805b3fd9a967929403391879a31d7c1844
Kristoffer Andersson <kristoffer.andersson@gu.se>
2023-05-10 15:13:22 +0200

    Bump version: 0.2.2 → 0.2.3

--- src/sblex/main/__init__.py
+++ src/sblex/main/__init__.py
@@ -2,7 +2,7 @@ from sblex.main import telemetry
 from sblex.main.bootstrap import bootstrap_app
 
 __all__ = ["bootstrap_app", "telemetry"]
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 
 def get_version() -> str:
