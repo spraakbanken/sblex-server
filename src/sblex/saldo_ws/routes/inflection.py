5a5e6a0b1965a2098a0ad53ab40c224cdc392af7
Kristoffer Andersson <kristoffer.andersson@gu.se>
2024-03-18 09:36:55 +0100

    fix: separate paradigm,word and data

--- src/sblex/saldo_ws/routes/inflection.py
+++ src/sblex/saldo_ws/routes/inflection.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends, Request
 from fastapi.responses import HTMLResponse, ORJSONResponse
 from opentelemetry import trace
 from pydantic.dataclasses import dataclass
+from sblex.application import queries
 from sblex.application.queries.inflection import InflectionTableQuery
 from sblex.saldo_ws import deps, schemas, templating
 
@@ -47,7 +48,7 @@ async def inflection_table_html(
         templates = request.app.state.templates
         if len(json_data) > 0:
             title = f'{json_data[0]["p"]} "{json_data[0]["gf"]}"'
-            data = prepare_for_html(json_data, word=word, paradigm=paradigm)
+            data = prepare_for_html(json_data)
         else:
             title = f'{paradigm} "{word}"'
             data = None
@@ -60,6 +61,7 @@ async def inflection_table_html(
                 service="gen",
                 show_bar=False,
                 paradigm=paradigm,
+                word=word,
                 data=data,
             ),
         )
@@ -72,15 +74,13 @@ class InflectionByMsd:
 
 
 @dataclass
-class ParadigmWord:
-    word: str
-    paradigm: str
+class ParadigmWordData:
     pos: str
     inflections_by_msd: list[InflectionByMsd]
     inherent_features: str | None = None
 
 
-def prepare_for_html(json_data: list[dict], *, word: str, paradigm: str) -> ParadigmWord:
+def prepare_for_html(json_data: list[queries.InflectionTableRow]) -> ParadigmWordData:
     with trace.get_tracer(__name__).start_as_current_span(
         sys._getframe().f_code.co_name
     ) as _process_api_span:
@@ -92,20 +92,18 @@ def prepare_for_html(json_data: list[dict], *, word: str, paradigm: str) -> Para
         forms_by_msd = group_msd(json_data)
         inflections_by_msd = collect_by_msd(json_data, forms_by_msd)
 
-        return ParadigmWord(
-            word=word,
-            paradigm=paradigm,
+        return ParadigmWordData(
             pos=head_word["pos"],
             inherent_features=inherent_features,
             inflections_by_msd=inflections_by_msd,
         )
 
 
-def group_msd(j: list[dict]) -> dict[str, list[str]]:
+def group_msd(j: list[queries.InflectionTableRow]) -> dict[str, list[str]]:
     with trace.get_tracer(__name__).start_as_current_span(
         sys._getframe().f_code.co_name
     ) as _process_api_span:
-        grouped_msd = {}
+        grouped_msd: dict[str, list[str]] = {}
         for x in j:
             msd = x["msd"]
             wf = x["form"]
@@ -117,7 +115,7 @@ def group_msd(j: list[dict]) -> dict[str, list[str]]:
 
 
 def collect_by_msd(
-    json_data: list[dict], forms_by_msd: dict[str, list[str]]
+    json_data: list[queries.InflectionTableRow], forms_by_msd: dict[str, list[str]]
 ) -> list[InflectionByMsd]:
     with trace.get_tracer(__name__).start_as_current_span(
         sys._getframe().f_code.co_name
