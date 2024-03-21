e586862fee51700249a7c2263256b26c1bd210fe
Kristoffer Andersson <kristoffer.andersson@gu.se>
2024-03-21 10:12:15 +0100

    fix: temporarily disable sms routes
    See tracking issue #110

--- tests/e2e/saldo_ws/test_compounds.py
+++ tests/e2e/saldo_ws/test_compounds.py
@@ -13,7 +13,7 @@ def snapshot_json(snapshot):
 @pytest.mark.asyncio
 async def test_sms_json(segment: str, client: AsyncClient, snapshot_json) -> None:
     response = await client.get(f"/sms/json/{segment}")
-    assert response.status_code == status.HTTP_200_OK
+    assert response.status_code == status.HTTP_503_SERVICE_UNAVAILABLE
     assert response.json() == snapshot_json
 
 
@@ -21,7 +21,7 @@ async def test_sms_json(segment: str, client: AsyncClient, snapshot_json) -> Non
 @pytest.mark.asyncio
 async def test_sms_html(segment: str, client: AsyncClient, snapshot) -> None:
     response = await client.get(f"/sms/html/{segment}")
-    assert response.status_code == status.HTTP_200_OK
+    assert response.status_code == status.HTTP_503_SERVICE_UNAVAILABLE
     assert response.text == snapshot
 
 
@@ -29,6 +29,6 @@ async def test_sms_html(segment: str, client: AsyncClient, snapshot) -> None:
 @pytest.mark.asyncio
 async def test_sms_xml(segment: str, client: AsyncClient, snapshot) -> None:
     response = await client.get(f"/sms/xml/{segment}")
-    assert response.status_code == status.HTTP_200_OK
+    assert response.status_code == status.HTTP_503_SERVICE_UNAVAILABLE
     assert response.headers["content-type"] == "application/xml"
     assert response.text == snapshot
