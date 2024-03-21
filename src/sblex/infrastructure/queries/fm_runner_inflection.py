f9c4c814244d6be328311ed6ef5269f57b4e5444
Kristoffer Andersson <kristoffer.andersson@gu.se>
2024-03-21 13:24:29 +0100

    refactor: add tracing to all routes

--- src/sblex/infrastructure/queries/fm_runner_inflection.py
+++ src/sblex/infrastructure/queries/fm_runner_inflection.py
@@ -1,3 +1,6 @@
+import sys
+
+from opentelemetry import trace
 from sblex.application.queries import InflectionTableQuery
 from sblex.application.queries.inflection import InflectionTableRow
 from sblex.fm import FmRunner
@@ -9,14 +12,17 @@ class FmRunnerInflectionTable(InflectionTableQuery):
         self.fm_runner = fm_runner
 
     def query(self, paradigm: str, word: str) -> list[InflectionTableRow]:
-        return [
-            {
-                "form": row["word"],
-                "gf": row["head"],
-                "pos": row["pos"],
-                "inhs": row["inhs"],
-                "msd": row["param"],
-                "p": row["p"],
-            }
-            for row in self.fm_runner.inflection(paradigm, word)
-        ]
+        with trace.get_tracer(__name__).start_as_current_span(
+            sys._getframe().f_code.co_name
+        ) as _process_api_span:
+            return [
+                {
+                    "form": row["word"],
+                    "gf": row["head"],
+                    "pos": row["pos"],
+                    "inhs": row["inhs"],
+                    "msd": row["param"],
+                    "p": row["p"],
+                }
+                for row in self.fm_runner.inflection(paradigm, word)
+            ]
