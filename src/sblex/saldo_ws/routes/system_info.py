e994061557de999ae484d809198ba43935d8d4fa
Kristoffer Andersson <kristoffer.andersson@gu.se>
2024-03-27 15:22:09 +0100

    chore(release): prepare for release

--- src/sblex/saldo_ws/routes/system_info.py
+++ src/sblex/saldo_ws/routes/system_info.py
@@ -8,12 +8,16 @@ from sblex.saldo_ws import schemas
 router = APIRouter()
 
 
+__version__ = "0.2.3"
+__date__ = "2023-05-10"
+
+
 @router.get("/version", response_model=schemas.Version, name="system_info:version")
 async def get_version():
     with trace.get_tracer(__name__).start_as_current_span(
         sys._getframe().f_code.co_name
     ) as _process_api_span:
-        return {"version": "0.2.3"}
+        return {"version": __version__, "date": __date__}
 
 
 @router.get("/version/json")
