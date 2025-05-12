1bd2b3cceb269c957cdc998053b2e5341918678f
Kristoffer Andersson <kristoffer.andersson@gu.se>
2025-05-12 15:31:43 +0200

    test: make tests clearer

--- tests/unit/test_schemas.py
+++ tests/unit/test_schemas.py
@@ -1,7 +1,6 @@
 import pydantic
 import pytest
 
-from sblex.application.predicates import is_lemma, is_lexeme
 from sblex.saldo_ws.schemas import Lemma, Lexeme
 
 
@@ -13,19 +12,23 @@ class WithLexeme(pydantic.BaseModel):
     a: Lexeme
 
 
-@pytest.mark.parametrize("word", ["lemma..nn.1", "lexeme..1"])
+@pytest.mark.parametrize("word", ["lemma..nn.1"])
 def test_lemma_schema_work(word: str) -> None:
-    if is_lemma(word):
+    _lemma = WithLemma(a=word)
+
+
+@pytest.mark.parametrize("word", ["lexeme..1"])
+def test_non_lemma_raises_validation_error(word: str) -> None:
+    with pytest.raises(pydantic.ValidationError):
         _lemma = WithLemma(a=word)
-    else:
-        with pytest.raises(pydantic.ValidationError):
-            _lemma = WithLemma(a=word)
 
 
-@pytest.mark.parametrize("word", ["lemma..nn.1", "lexeme..1"])
+@pytest.mark.parametrize("word", ["lexeme..1"])
 def test_lexeme_schema_work(word: str) -> None:
-    if is_lexeme(word):
+    _lemma = WithLexeme(a=word)
+
+
+@pytest.mark.parametrize("word", ["lemma..nn.1"])
+def test_non_lexeme_raises_validation_error(word: str) -> None:
+    with pytest.raises(pydantic.ValidationError):
         _lemma = WithLexeme(a=word)
-    else:
-        with pytest.raises(pydantic.ValidationError):
-            _lemma = WithLexeme(a=word)
